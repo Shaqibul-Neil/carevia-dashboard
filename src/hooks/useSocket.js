d8a7d35514dedd19be369655efda1dbb43416295
shaqibul-neil <shaqibul.islam.neil@gmail.com>
2026-02-15 19:45:35 +0600

    removed if no socket info throw error from use socket hook to prevent page reload error as react error boundary was catching it and instead now it will show a loading

--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -3,7 +3,7 @@ import SocketContext from "../context/SocketContext";
 
 const useSocket = () => {
   const socketInfo = useContext(SocketContext);
-  if (!socketInfo) throw new Error("Failed to connect Socket");
+
   return socketInfo;
 };
 export default useSocket;
